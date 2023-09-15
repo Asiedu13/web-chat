2087731b6fa2acb0cbe634a871a7be81c0a1ae6b
Prince Asiedu <princekofasiedu@gmail.com>
2023-09-15 13:14:51 +0000

    edited the UI

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -17,9 +17,14 @@
     </head>
     <body class="antialiased">
       {{-- Status section --}}
+      
 
       {{-- Chat section --}}
-      <main class="w-[100vw] h-[100vh] flex flex-col border-solid border-2 border-slate-200 items-end">
+      <main class="w-[100vw] h-[100vh] flex justify-around items-center border-solid border-2 border-slate-200 items-end">
+        <div class=" h-[400px] w-[600px] flex flex-col justify-center items-center">
+            <h2 class='text-5xl font-bold text-orange-200'>KingsChat</h2>
+            <p class="text-stone-400">Conversations with your loved ones in new way</p>
+        </div>
         <div class="rounded-xl w-[700px] h-[700px] border-solid border-2 border-slate-200 my-[10px] mx-[10px] ">
             <header class="border-solid border-y-2 border-slate-200 h-[85px] p-[1rem]">
                 <h2>{{$username}}</h2>
@@ -29,19 +34,12 @@
             {{-- Chats display --}}
             <section id='display-chat' class=" bg-orange-200 w-[696px] h-[540px] flex flex-col overflow-auto ">
                 
-                {{-- <section id='you' class='w-[200px] bg-orange-500'>
-
-                </section>
-
-                <section id='me' class="bg-orange-500 w-[200px]">
-
-                </section> --}}
             </section>
             
             {{-- Input section --}}
             <section>
                 <div class="flex">
-                        <input id ='message' class="h-[70px] w-[580px] p-[1rem]" type="text" name='message' placeholder="type here..."  />
+                        <input id ='message' class="h-[70px] w-[580px] p-[1rem] focus:outline-none" type="text" name='message' placeholder="type here..."  />
                         
                         <button id='send' class="border-solid border-x-2 border-slate-200 flex-1">Send</button>
                 </div>
